1d36cef7b7675df651b8f8dbebe58256e6d5f158
ArtyomSinyugin <74111551+ArtyomSinyugin@users.noreply.github.com>
2023-01-31 10:38:24 +0300

    Update main.rs

--- src/trait-object/var-2/main.rs
+++ src/trait-object/var-2/main.rs
@@ -1,23 +1,16 @@
-use design_pattern::Post;
-fn main () {
+use trait-object::Post;
+
+fn main() {
     let mut post = Post::new();
-    
-    post
-        .add_text("I ate a salad for lunch today");
-    //assert_eq!("", post.content());  // для варианта с типаж-объектами
 
-    let post = post
-        .request_review();
-    //assert_eq!("", post.content());  // для варианта с типаж-объектами
+    post.add_text("I ate a salad for lunch today");
+    assert_eq!("", post.content());
 
-    let mut post = post.reject();
+    post.request_review();
+    assert_eq!("", post.content());
+    //post.add_text(" .Valeria!");
 
-    post.add_text(". Valeria!");
-
-    let post = post.request_review();
-    let post = post.approve();
-    let post = post.approve();
-    //post.add_text("Wow");
-
-    assert_eq!("I ate a salad for lunch today. Valeria!", post.content());
+    post.approve();
+    post.approve();
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
