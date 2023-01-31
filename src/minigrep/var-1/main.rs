e853d924194642fa614b891e954ca7dc20556670
ArtyomSinyugin <74111551+ArtyomSinyugin@users.noreply.github.com>
2023-01-31 11:02:13 +0300

    Update main.rs

--- src/minigrep/var-1/main.rs
+++ src/minigrep/var-1/main.rs
@@ -4,19 +4,14 @@ use std::process;
 
 use minigrep::Config;  // если подставить вместо "use minigrep::*;", то run() не нужно вызывать таким сложным способом
 fn main() {
-    let config = Config::build(env::args()).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);   // без этой строчки unwrap_or_else не работает. Работает просто unwrap, но криво
-    });
-
-/*    // старая версия         
+     
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);   // без этой строчки unwrap_or_else не работает. Работает просто unwrap, но криво
     });
-*/     
+    
     // println!("Searching for {}", config.query);
     // println!("In file {}", config.file_path);
 
